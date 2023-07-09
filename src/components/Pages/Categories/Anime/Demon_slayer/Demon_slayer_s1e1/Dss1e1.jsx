4014036a104928140cd87206629e72f05c530afd
Niyaz Hasanov <116659469+niyazhsnov@users.noreply.github.com>
2023-07-10 02:14:18 +0400

    Add files via upload

--- src/components/Pages/Categories/Anime/Demon_slayer/Demon_slayer_s1e1/Dss1e1.jsx
+++ src/components/Pages/Categories/Anime/Demon_slayer/Demon_slayer_s1e1/Dss1e1.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 const Dss1e1 = () => {
   return (
     <div>
-    <li> high qualiy<iframe src="https://drive.google.com/file/d/1-fRJISsOyKf577BAPNO7-y9fRiREbBBJ/preview" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"  width="640" height="480" allow="autoplay"></iframe>
-    </li>
-    <li>low quality <iframe src="https://drive.google.com/file/d/1-JcRsKE2rSHg3kAnGm-rZ9btpHIoYFo3/preview"  allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"  width="640" height="480" allow="autoplay"></iframe>
-    </li>
+     <iframe src="https://drive.google.com/file/d/1-fRJISsOyKf577BAPNO7-y9fRiREbBBJ/preview" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen"  width="640" height="480" allow="autoplay"></iframe>
+   
    </div>
   )
 }
